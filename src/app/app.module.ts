69b3883fab41418ad975d1a58cb7c9dc6fe02e24
Ozmrcr <ozmercer@gmail.com>
2020-05-16 11:34:01 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClaimScreenComponent } from './claim-screen/claim-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClaimScreenComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/claim-screen/claim-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-claim-screen',
+  templateUrl: './claim-screen.component.html',
+  styleUrls: ['./claim-screen.component.sass']
+})
+export class ClaimScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
