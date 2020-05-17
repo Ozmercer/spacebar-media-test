391fa8b0f6d8b473e0a9e15ad0b37f3497c5e9d3
Ozmrcr <ozmercer@gmail.com>
2020-05-17 14:59:08 +0300

    Final tweaks and design improvements

--- src/app/pipes/seconds-to-timer.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SecondsToTimerPipe } from './seconds-to-timer.pipe';
-
-describe('SecondsToTimerPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SecondsToTimerPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
