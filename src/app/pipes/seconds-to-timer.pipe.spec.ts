38e7e5a4ead1b5a6fa8016823f69301959d9444a
Ozmrcr <ozmercer@gmail.com>
2020-05-18 16:22:14 +0300

    Add tests

--- src/app/pipes/seconds-to-timer.pipe.spec.ts
+++ src/app/pipes/seconds-to-timer.pipe.spec.ts
@@ -7,6 +7,7 @@ describe('SecondsToTimerPipe', () => {
     expect(pipe.transform(660)).toEqual('11:00');	
     expect(pipe.transform(60)).toEqual('01:00');	
     expect(pipe.transform(25)).toEqual('00:25');	
+    expect(pipe.transform(5)).toEqual('00:05');	
     expect(pipe.transform(90)).toEqual('01:30');	
   });	
 });
